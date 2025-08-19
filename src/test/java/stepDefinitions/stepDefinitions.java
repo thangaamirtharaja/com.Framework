46ff746a5e99c01e3767807204d11a63caa333ef
Gold Raja <thangaamirtharajav@gmail.com>
2025-08-19 11:36:02 +0530

    Test evidance

--- src/test/java/stepDefinitions/stepDefinitions.java
+++ src/test/java/stepDefinitions/stepDefinitions.java
@@ -12,30 +12,56 @@ import utils.Common_config.WaitForPageLoad;
 import utils.Reports.WordEvidenceGenerator;
 
 
-public class stepDefinitions{
+public class stepDefinitions {
     WaitForPageLoad waitforpageload = new WaitForPageLoad();
+
     @Given("I navigate to OrangeHRM website")
     public void i_navigate_to_orange_hrm_website() throws InterruptedException, IOException {
-
         DriverManager.getDriver().get(ConfigReader.get("Env"));
         DriverManager.getDriver().manage().timeouts()
                 .implicitlyWait(Duration.ofSeconds(Integer.parseInt(ConfigReader.get("implicitWait"))));
-        waitforpageload.waitforpageload(DriverManager.getDriver(),Integer.parseInt(ConfigReader.get("Wait")));
+        waitforpageload.waitforpageload(DriverManager.getDriver(), Integer.parseInt(ConfigReader.get("Wait")));
         Thread.sleep(5000);
-//        String screenshotPath = ScreenshotManager.takeStepScreenshot(DriverManager.getDriver()); // your method
-//        WordEvidenceGenerator.addStep("User clicks on login button", "PASS", screenshotPath);
+    }
+
+    @Given("I navigate to OrangeHRM website2")
+    public void i_navigate_to_orange_hrm_website2() throws InterruptedException, IOException {
+        System.out.println("1");
 
     }
     @Given("I navigate to OrangeHRM website1")
     public void i_navigate_to_orange_hrm_website1() throws InterruptedException, IOException {
         System.out.println("1");
-//        DriverManager.getDriver().get(ConfigReader.get("Env"));
-//        DriverManager.getDriver().manage().timeouts()
-//                .implicitlyWait(Duration.ofSeconds(Integer.parseInt(ConfigReader.get("implicitWait"))));
-//        waitforpageload.waitforpageload(DriverManager.getDriver(),Integer.parseInt(ConfigReader.get("Wait")));
-//        Thread.sleep(5000);
-//        String screenshotPath = ScreenshotManager.takeStepScreenshot(DriverManager.getDriver()); // your method
-//        WordEvidenceGenerator.addStep("User clicks on login button", "PASS", screenshotPath);
+
+    }
+
+    @Given("I navigate to OrangeHRM website3")
+    public void i_navigate_to_orange_hrm_website3() throws InterruptedException, IOException {
+        System.out.println("1");
+
+    }
+
+    @Given("I navigate to OrangeHRM website4")
+    public void i_navigate_to_orange_hrm_website4() throws InterruptedException, IOException {
+        System.out.println("1");
+
+    }
+
+    @Given("I navigate to OrangeHRM website5")
+    public void i_navigate_to_orange_hrm_website5() throws InterruptedException, IOException {
+        System.out.println("1");
+
+    }
+
+    @Given("I navigate to OrangeHRM website6")
+    public void i_navigate_to_orange_hrm_website6() throws InterruptedException, IOException {
+        System.out.println("1");
+
+    }
+
+    @Given("I navigate to OrangeHRM website7")
+    public void i_navigate_to_orange_hrm_website7() throws InterruptedException, IOException {
+        System.out.println("1");
 
     }
 
--- src/test/resources/features/cucumber.feature
+++ src/test/resources/features/cucumber.feature
@@ -3,21 +3,13 @@ Feature: Login to OrangeHRM
   Scenario: Valid login
     Given I navigate to OrangeHRM website
     Given I navigate to OrangeHRM website1
-    Given I navigate to OrangeHRM website1
-    Given I navigate to OrangeHRM website1
-    Given I navigate to OrangeHRM website1
-    Given I navigate to OrangeHRM website1
-    Given I navigate to OrangeHRM website1
-    Given I navigate to OrangeHRM website1
-    Given I navigate to OrangeHRM website1
-    Given I navigate to OrangeHRM website1
-    Given I navigate to OrangeHRM website1
-    Given I navigate to OrangeHRM website1
-    Given I navigate to OrangeHRM website1
-    Given I navigate to OrangeHRM website1
-    Given I navigate to OrangeHRM website1
-    Given I navigate to OrangeHRM website1
-    Given I navigate to OrangeHRM website1
+    Given I navigate to OrangeHRM website2
+    Given I navigate to OrangeHRM website3
+    Given I navigate to OrangeHRM website4
+    Given I navigate to OrangeHRM website5
+    Given I navigate to OrangeHRM website6
+    Given I navigate to OrangeHRM website7
+
 
     
  
